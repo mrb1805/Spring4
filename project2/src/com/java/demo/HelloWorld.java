8488f4655cbf106eea79779e6fd7c2c362bea3bd
mrb1805 <maahesh.rb@gmail.com>
2016-09-27 00:08:28 -0700

    Project 2 - Demo Bank

---
+++ project2/src/com/java/demo/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.java.demo;
+
+public class HelloWorld implements HelloWorldInterface {
+	public String sayHello(String name){
+		return "Hello "+name;
+	}
+
+}
---
+++ project2/src/com/java/demo2/SpellChecker.java
@@ -0,0 +1,8 @@
+package com.java.demo2;
+
+public class SpellChecker {
+	
+	public void checkSpelling(String document){
+		System.out.println("Spelling check completed for the given "+ document);
+	}
+}
